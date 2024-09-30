03581df04282f26c4fd6ac6567b7b53514cefab5
dpa313 <dpa.github@gmail.com>
2024-09-30 16:15:03 +0600

    weather-forecast-added

--- script.js
+++ script.js
@@ -1,27 +1,90 @@
-const searchInput = document.querySelector(".search-input")
+const searchInput = document.querySelector(".search-input");
+const currentWeatherDiv = document.querySelector(".current-weather");
+const houlryWeatherDiv = document.querySelector(".hourly-weather .weather-list");
 
-const API_KEY = "b71f2c6d324e410893494324242909"
+const API_KEY = "b71f2c6d324e410893494324242909";
 
-const getWeatherDetails = async (cityName) =>{
-    const API_URL = `http://api.weatherapi.com/v1/forecast.json?key=${API_KEY}&q=${cityName}`;
+const weatherCodes = {
+  clear: [1000],
+  clouds: [1003, 1006, 1009],
+  mist: [1030, 1145, 1147],
+  rain: [
+    1063, 1150, 1153.1168, 1171, 1181, 1183, 1198, 1201, 1240, 1243, 1246, 1273,
+    1276,
+  ],
+  moderate_heavy_rain: [1186, 1189, 1195, 1243, 1246],
+  snow: [
+    1066, 1069, 1072, 1114, 1117, 1204, 1207, 1210, 1213, 1216, 1219, 1222,
+    1225, 1237, 1249, 1252, 1255, 1258, 1261, 1264.1279, 1282,
+  ],
+  thunder: [1087, 1279, 1282],
+  thunder_ran: [1273, 1276],
+};
 
-    try{
-        // fetch data from api and parse the response as json
-        const response = await fetch(API_URL)
-        const data = await response.json()
-        
-        console.log(data);
-    }
-    catch(error){
-        console.log(error);
-    }
-}
+const displayHourlyForecast = (hourlyData) => {
+  const currentHour = new Date().setMinutes(0, 0, 0);
+  const next24Hour = currentHour + 24 * 60 * 60 * 1000;
+
+  const next24HourData = hourlyData.filter(({ time }) => {
+    forecastTime = new Date(time).getTime();
+    return forecastTime >= currentHour && forecastTime <= next24Hour;
+  });
+
+  houlryWeatherDiv.innerHTML = next24HourData.map((item) => {
+    const temperature = Math.floor(item.temp_c);
+    const time = item.time.split(" ")[1].substring(0,5)
+    const weatherIcon = Object.keys(weatherCodes).find((icon) =>
+      weatherCodes[icon].includes(item.condition.code)
+    );
+
+    return `<li class="weather-item">
+              <p class="time">${time}</p>
+              <img src="/icons/${weatherIcon}.svg" alt="" class="weather-icon" />
+              <p class="temperature">${temperature}°</p>
+            </li>`;
+  }).join("")
+
+  console.log(hourlyWeatherHTML);
+};
+
+const getWeatherDetails = async (cityName) => {
+  const API_URL = `http://api.weatherapi.com/v1/forecast.json?key=${API_KEY}&q=${cityName}&days=2`;
+
+  try {
+    // fetch data from api and parse the response as json
+    const response = await fetch(API_URL);
+    const data = await response.json();
+
+    const temperature = Math.floor(data.current.temp_c);
+    const description = data.current.condition.text;
+    const weatherIcon = Object.keys(weatherCodes).find((icon) =>
+      weatherCodes[icon].includes(data.current.condition.code)
+    );
+
+    currentWeatherDiv.querySelector(
+      ".weather-icon"
+    ).src = `/icons/${weatherIcon}.svg`;
+    currentWeatherDiv.querySelector(
+      ".temperature"
+    ).innerHTML = `${temperature}<span>°C</span>`;
+    currentWeatherDiv.querySelector(".description").innerText = description;
+
+    // combined hourly data from today and tomorrow
+    const combinedHourlyData = [
+      ...data.forecast.forecastday[0].hour,
+      ...data.forecast.forecastday[1].hour,
+    ];
+    displayHourlyForecast(combinedHourlyData);
+  } catch (error) {
+    console.log(error);
+  }
+};
 
 // Handle user input in the searchbox
-searchInput.addEventListener('keyup', (e) =>{
-    const cityName = searchInput.value.trim()
+searchInput.addEventListener("keyup", (e) => {
+  const cityName = searchInput.value.trim();
 
-    if(e.key == "Enter" && cityName){
-        getWeatherDetails(cityName)
-    }
-})
+  if (e.key == "Enter" && cityName) {
+    getWeatherDetails(cityName);
+  }
+});
